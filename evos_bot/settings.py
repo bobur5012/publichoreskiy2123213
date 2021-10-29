b7da4e0fbcbadd3179e9b028709f7ce5b5d731ef
doniyor <doniyorkarimoff96@gmail.com>
2021-10-29 12:49:42 +0500

    deploy

--- evos_bot/settings.py
+++ evos_bot/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'evos_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
